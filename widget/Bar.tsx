4cccc7b01ac958f4c2da13467f65b57411283307
Melker <melkerroos@outlook.com>
2025-08-12 03:28:01 +0200

    hi again

--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -1,11 +1,18 @@
-import app from "ags/gtk4/app"
-import {Gtk, Gdk, Astal} from "ags/gtk4"
+//import app from "ags/gtk4/app"
+import {Gtk, Gdk, Astal, App} from "ags/gtk4"
 import {createPoll} from "ags/time"
+import { Variable } from "astal";
+
+
+function doCoolStuff() {
+    //////
+    console.log("hey!!!!");
+  }
 
 export default function Bar(monitor = 0) {
   const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
   const clock = createPoll("", 60000, "date +%H:%M")
-  const hyprland = Hyprland.get_default()
+
 
   return (
   <window 
@@ -24,9 +31,17 @@ export default function Bar(monitor = 0) {
         halign={Gtk.Align.CENTER}
         />
 </button>
+  <eventbox 
+        onButtonPressEvent={() => {
+          doCoolStuff();
+        }}
+      halign={Gtk.Align.LEFT}
+    >
+      <label label="mysterious button" />
+    </eventbox> 
   </window>
 
   
-  )
+  );
 }
 
